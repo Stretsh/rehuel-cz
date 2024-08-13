7936078a8754cfdad449ceab910623b846637c2e
Stretsh <stretsh@gmail.com>
2024-08-13 11:27:17 +0200

    custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import icon from 'astro-icon'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  site: 'https://stretsh.github.io',
-  base: 'rehuel-cz',
+  site: 'https://rehuel.cz',
 })
